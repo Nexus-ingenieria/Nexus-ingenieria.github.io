c42065b1e5bb94f8b6ee48677953a132888d2cec
Nexus-ingenieria <seedsenglishschoolcantarero@gmail.com>
2025-12-02 13:06:51 -0600

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,8 +1,1 @@
-<script>
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('nav ul');
 
-hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('show');
-});
-</script>
