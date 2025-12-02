b3e04c07e788a1da2de5b05c72e8bf77be7893ef
Nexus-ingenieria <seedsenglishschoolcantarero@gmail.com>
2025-12-02 13:01:00 -0600

    Create menu.js

---
+++ menu.js
@@ -0,0 +1,8 @@
+<script>
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('nav ul');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+});
+</script>
